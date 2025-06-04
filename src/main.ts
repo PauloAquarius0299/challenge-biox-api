98735a29a61a60bd8f197c7eaf4da0cd2f9a6cb2
Paulo Cesar <paulocgsantos99@gmail.com>
2025-06-04 12:11:54 -0300

    feat: add main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { RecipesModule } from './recipe.module';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(RecipesModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
